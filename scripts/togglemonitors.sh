42d02366944e6cd8661c7355a52d61d2d19778a6
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-06 22:59:06 +0200

    Add monitor resolution to display toogling menu

--- scripts/togglemonitors.sh
+++ scripts/togglemonitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wlr-randr | grep -E '"*"|Enabled' | awk '{if ($1 ~ "Enabled") print $2 "@"; else print $1 " "}' | tr -d '\n' | tr '@' '\n' | fuzzel -d --log-level none --width 15 --lines 5 | awk '{print $1}' | xargs -I _ wlr-randr --output _ --toggle
+wlr-randr | grep -E '"*"|Enabled|preferred' | awk '{if ($1 ~ "Enabled") print $2 " "; else if ($1 ~ /[0-9]+x[0-9]+/) print $1 "@"; else print $1 " "}' | tr -d '\n' | tr '@' '\n' | fuzzel -d --log-level none --width 25 --lines 5 | awk '{print $1}' | xargs -I _ wlr-randr --output _ --toggle
