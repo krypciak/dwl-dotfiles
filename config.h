e756dcc62b44405ea4171484a5f68b7b75d88961
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-08 00:57:15 +0200

    Add keycodes

--- config.h
+++ config.h
@@ -166,18 +166,18 @@ LIBINPUT_CONFIG_TAP_MAP_LMR -- 1/2/3 finger tap maps to left/middle/right
 */
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
-#define TAGKEYS(KEY,SKEY,TAG) \
-	{ ALT,                KEY,            view,            {.ui = 1 << TAG} }, \
-	{ ALT|CTRL,           KEY,            toggleview,      {.ui = 1 << TAG} }, \
-	{ ALT|SHIFT,          SKEY,           tag,             {.ui = 1 << TAG} }, \
-	{ ALT|CTRL|SHIFT,     SKEY,           toggletag,       {.ui = 1 << TAG} }
+#define TAGKEYS(KEY,TAG) \
+	{ ALT,                KEY,           view,            {.ui = 1 << TAG} }, \
+	{ ALT|CTRL,           KEY,           toggleview,      {.ui = 1 << TAG} }, \
+	{ ALT|SHIFT,          KEY,           tag,             {.ui = 1 << TAG} }, \
+	{ ALT|CTRL|SHIFT,     KEY,           toggletag,       {.ui = 1 << TAG} }
 
 
 
 #define PLAYERCTL_SCRIPT "luajit $HOME/.config/dotfiles/scripts/playerctl.lua "
-#define PLAYERCTL_KEY(KEY,SHIFT_KEY,ACTION) \
-	{ CAPS,                    KEY,                 simplespawn,     {.v = PLAYERCTL_SCRIPT "\"" ACTION "\" 1"} }, \
-	{ CAPS|SHIFT,              SHIFT_KEY,           simplespawn,     {.v = PLAYERCTL_SCRIPT "\"" ACTION "\" 2"} }
+#define PLAYERCTL_KEY(KEY,ACTION) \
+	{ CAPS,                    KEY,           simplespawn,     {.v = PLAYERCTL_SCRIPT "\"" ACTION "\" 1"} }, \
+	{ CAPS|SHIFT,              KEY,           simplespawn,     {.v = PLAYERCTL_SCRIPT "\"" ACTION "\" 2"} }
 
 
 /* Autostart */
@@ -213,157 +213,155 @@ static const char *const at_exit[] = {
     "pkill gnome-keyring",
 };
 
-
-
+#include "keys.h"
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-    { SUPER|CTRL,               XKB_KEY_Return,     zoom,           {0} },
+    { SUPER|CTRL,               Key_Return,     zoom,           {0} },
 	
     // monitor
-	{ ALT,                      XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-	{ ALT,                      XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-	{ ALT|SHIFT,                XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-	{ ALT|SHIFT,                XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
+	{ ALT,                      Key_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
+	{ ALT,                      Key_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
+	{ ALT|SHIFT,                Key_comma,      tagmon,         {.i = WLR_DIRECTION_LEFT} },
+	{ ALT|SHIFT,                Key_period,     tagmon,         {.i = WLR_DIRECTION_RIGHT} },
 
-	{ SUPER,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-	{ SUPER,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-	{ SUPER|SHIFT,              XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-	{ SUPER|SHIFT,              XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
+	{ SUPER,                    Key_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
+	{ SUPER,                    Key_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
+	{ SUPER|SHIFT,              Key_comma,      tagmon,         {.i = WLR_DIRECTION_LEFT} },
+	{ SUPER|SHIFT,              Key_period,     tagmon,         {.i = WLR_DIRECTION_RIGHT} },
 
 
     // tags
-	TAGKEYS(                    XKB_KEY_Tab,        XKB_KEY_ISO_Left_Tab,    0),
-	TAGKEYS(                    XKB_KEY_q,          XKB_KEY_Q,      1),
-	TAGKEYS(                    XKB_KEY_w,          XKB_KEY_W,      2),
+	TAGKEYS(                    Key_Tab,        0),
+	TAGKEYS(                    Key_q,          1),
+	TAGKEYS(                    Key_w,          2),
     
-	TAGKEYS(                    XKB_KEY_n,          XKB_KEY_N,      3),
-	TAGKEYS(                    XKB_KEY_d,          XKB_KEY_D,      4),
-	TAGKEYS(                    XKB_KEY_s,          XKB_KEY_S,      5),
-	TAGKEYS(                    XKB_KEY_a,          XKB_KEY_A,      6),
-	TAGKEYS(                    XKB_KEY_z,          XKB_KEY_Z,      7),
-	TAGKEYS(                    XKB_KEY_t,          XKB_KEY_T,      8),
-	TAGKEYS(                    XKB_KEY_f,          XKB_KEY_F,      9),
-	TAGKEYS(                    XKB_KEY_c,          XKB_KEY_C,      10),
-	TAGKEYS(                    XKB_KEY_g,          XKB_KEY_G,      11),
-	TAGKEYS(                    XKB_KEY_v,          XKB_KEY_V,      12),
-	TAGKEYS(                    XKB_KEY_h,          XKB_KEY_H,      13),
+	TAGKEYS(                    Key_n,          3),
+	TAGKEYS(                    Key_d,          4),
+	TAGKEYS(                    Key_s,          5),
+	TAGKEYS(                    Key_a,          6),
+	TAGKEYS(                    Key_z,          7),
+	TAGKEYS(                    Key_t,          8),
+	TAGKEYS(                    Key_f,          9),
+	TAGKEYS(                    Key_c,          10),
+	TAGKEYS(                    Key_g,          11),
+	TAGKEYS(                    Key_v,          12),
+	TAGKEYS(                    Key_h,          13),
 
 
     // view all tags at once
-	{ SUPER,                    XKB_KEY_0,          view,           {.ui = ~0} },
+	{ SUPER,                    Key_0,          view,           {.ui = ~0} },
 
 
     // media
-    PLAYERCTL_KEY(XKB_KEY_Tab, XKB_KEY_ISO_Left_Tab, "play-pause"),
-    PLAYERCTL_KEY(XKB_KEY_q,   XKB_KEY_Q,            "next"),
-    PLAYERCTL_KEY(XKB_KEY_a,   XKB_KEY_A,            "previous"),
-    PLAYERCTL_KEY(XKB_KEY_w,   XKB_KEY_W,            "volume 0.02%+"),
-    PLAYERCTL_KEY(XKB_KEY_s,   XKB_KEY_S,            "volume 0.02%-"), 
+    PLAYERCTL_KEY(Key_Tab, "play-pause"),
+    PLAYERCTL_KEY(Key_q,   "next"),
+    PLAYERCTL_KEY(Key_a,   "previous"),
+    PLAYERCTL_KEY(Key_w,   "volume 0.02%+"),
+    PLAYERCTL_KEY(Key_s,   "volume 0.02%-"), 
 	
-    { CAPS|CTRL,                XKB_KEY_Tab,        simplespawn,     {.v = PLAYERCTL_SCRIPT "swap -1"} },
+    { CAPS|CTRL,                Key_Tab,        simplespawn,     {.v = PLAYERCTL_SCRIPT "swap -1"} },
 
 
-    { CAPS,                     XKB_KEY_e,          simplespawn,    {.v = "amixer set Master 5%+" } },
-    { CAPS,                     XKB_KEY_d,          simplespawn,    {.v = "amixer set Master 5%-" } },
+    { CAPS,                     Key_e,          simplespawn,    {.v = "amixer set Master 5%+" } },
+    { CAPS,                     Key_d,          simplespawn,    {.v = "amixer set Master 5%-" } },
 
-    { CAPS|SHIFT,               XKB_KEY_E,          simplespawn,    {.v = "amixer set Capture 5%+" } },
-    { CAPS|SHIFT,               XKB_KEY_D,          simplespawn,    {.v = "amixer set Capture 5%-" } },
-    { CAPS|CTRL,                XKB_KEY_e,          simplespawn,    {.v = "amixer set Capture cap" } },
-    { CAPS|CTRL,                XKB_KEY_d,          simplespawn,    {.v = "amixer set Capture nocap" } },
+    { CAPS|SHIFT,               Key_e,          simplespawn,    {.v = "amixer set Capture 5%+" } },
+    { CAPS|SHIFT,               Key_d,          simplespawn,    {.v = "amixer set Capture 5%-" } },
+    { CAPS|CTRL,                Key_e,          simplespawn,    {.v = "amixer set Capture cap" } },
+    { CAPS|CTRL,                Key_d,          simplespawn,    {.v = "amixer set Capture nocap" } },
 
     // select audio output
-    { CAPS|CTRL,                XKB_KEY_q,          simplespawn,    {.v = "pacmd set-default-sink \"$(pactl list sinks short | awk '{print $1 \" <> \" substr($2,13) }' | tr '-' ' ' | tr '_' ' ' | fuzzel -d --log-level=none --width 50 | awk '{print $1}')\"" } },
+    { CAPS|CTRL,                Key_q,          simplespawn,    {.v = "pacmd set-default-sink \"$(pactl list sinks short | awk '{print $1 \" <> \" substr($2,13) }' | tr '-' ' ' | tr '_' ' ' | fuzzel -d --log-level=none --width 50 | awk '{print $1}')\"" } },
 
 
     // gaps
-    { CAPS,                     XKB_KEY_y,          incgaps,        {.i = +1} },
-    { CAPS,                     XKB_KEY_h,          incgaps,        {.i = -1} },
+    { CAPS,                     Key_y,          incgaps,        {.i = +1} },
+    { CAPS,                     Key_h,          incgaps,        {.i = -1} },
     
-    { CAPS|SHIFT,               XKB_KEY_Y,          setgaps_single, {.i = default_gapps } },
-    { CAPS|SHIFT,               XKB_KEY_H,          setgaps_single, {.i = 0 } },
+    { CAPS|SHIFT,               Key_y,          setgaps_single, {.i = default_gapps } },
+    { CAPS|SHIFT,               Key_h,          setgaps_single, {.i = 0 } },
 
-    { CAPS|CTRL,                XKB_KEY_y,          setborder,      {.ui = default_gapps } },
-    { CAPS|CTRL,                XKB_KEY_h,          setborder,      {.ui = 0 } },
+    { CAPS|CTRL,                Key_y,          setborder,      {.ui = default_gapps } },
+    { CAPS|CTRL,                Key_h,          setborder,      {.ui = 0 } },
 
 
     // walpaper
-    { CAPS,                     XKB_KEY_t,          simplespawn,    {.v = WALLPAPER_SCRIPT "wayland-gui" } },
-    // { CAPS,                     XKB_KEY_g,          simplespawn,    {.v = WALLPAPER_SCRIPT "inc 0 1" } },
+    { CAPS,                     Key_t,          simplespawn,    {.v = WALLPAPER_SCRIPT "wayland-gui" } },
     
     // launcher
-	{ ALT,                      XKB_KEY_r,          simplespawn,    {.v = "fuzzel --width 30 --log-level=none" } },
-	{ ALT,                      XKB_KEY_Return,     simplespawn,    {.v = "alacritty" } },
+	{ ALT,                      Key_r,          simplespawn,    {.v = "fuzzel --width 30 --log-level=none" } },
+	{ ALT,                      Key_Return,     simplespawn,    {.v = "alacritty" } },
     // clipboard history view
-    { CAPS,                     XKB_KEY_1,          simplespawn,    {.v = "cliphist list | fuzzel --width 100 -d --log-level=none | cliphist decode | wl-copy" } },
+    { CAPS,                     Key_1,          simplespawn,    {.v = "cliphist list | fuzzel --width 100 -d --log-level=none | cliphist decode | wl-copy" } },
     // clear the clipboard history
-    { CAPS|CTRL,                XKB_KEY_1,          simplespawn,    {.v = "rm $HOME/.cache/cliphist/db" } },
+    { CAPS|CTRL,                Key_1,          simplespawn,    {.v = "rm $HOME/.cache/cliphist/db" } },
 
-    { SUPER|ALT,                XKB_KEY_s,          simplespawn,    {.v = "pkill steam" } },
-    { SUPER|ALT,                XKB_KEY_v,          simplespawn,    {.v = "pkill -9 gamescope; pkill Riot; pkill League; pkill lutris; pkill wine; pkill wine-preloader; pkill wine-device; pkill plugplay; pkill services.exe; pkill rpcss.exe; pkill svchost.exe; pkill gamescope" } },
-    { SUPER|ALT,                XKB_KEY_y,          simplespawn,    {.v = "pkill lbry" } },
-    { SUPER|ALT,                XKB_KEY_z,          simplespawn,    {.v = "pkill tutanota" } },
-    { SUPER|ALT,                XKB_KEY_u,          simplespawn,    {.v = "pkill gammastep" } },
-    { SUPER|ALT,                XKB_KEY_k,          simplespawn,    {.v = "pkill keepassxc" } },
-    { SUPER|ALT,                XKB_KEY_d,          simplespawn,    {.v = "pkill discord" } },
+    { SUPER|ALT,                Key_s,          simplespawn,    {.v = "pkill steam" } },
+    { SUPER|ALT,                Key_v,          simplespawn,    {.v = "pkill -9 gamescope; pkill Riot; pkill League; pkill lutris; pkill wine; pkill wine-preloader; pkill wine-device; pkill plugplay; pkill services.exe; pkill rpcss.exe; pkill svchost.exe; pkill gamescope" } },
+    { SUPER|ALT,                Key_y,          simplespawn,    {.v = "pkill lbry" } },
+    { SUPER|ALT,                Key_z,          simplespawn,    {.v = "pkill tutanota" } },
+    { SUPER|ALT,                Key_u,          simplespawn,    {.v = "pkill gammastep" } },
+    { SUPER|ALT,                Key_k,          simplespawn,    {.v = "pkill keepassxc" } },
+    { SUPER|ALT,                Key_d,          simplespawn,    {.v = "pkill discord" } },
 
-	{ CAPS,                     XKB_KEY_b,          getclientinfo,  {0} },
-	{ CAPS,                     XKB_KEY_3,          simplespawn,    {.v = "$HOME/.config/dwl/dwl-dotfiles/scripts/togglemonitors.sh" } },
+	{ CAPS,                     Key_b,          getclientinfo,  {0} },
+	{ CAPS,                     Key_3,          simplespawn,    {.v = "$HOME/.config/dwl/dwl-dotfiles/scripts/togglemonitors.sh" } },
 
 
     // layout 
     // master count
-	{ SUPER|SHIFT,              XKB_KEY_L,          incnmaster,     {.i = -1} },
-	{ SUPER|SHIFT,              XKB_KEY_H,          incnmaster,     {.i = +1} },
+	{ SUPER|SHIFT,              Key_l,          incnmaster,     {.i = -1} },
+	{ SUPER|SHIFT,              Key_h,          incnmaster,     {.i = +1} },
     // master width
-	{ SUPER,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
-	{ SUPER,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
+	{ SUPER,                    Key_h,          setmfact,       {.f = -0.05} },
+	{ SUPER,                    Key_l,          setmfact,       {.f = +0.05} },
     // layout switching
-    { CAPS,                    XKB_KEY_r,           cyclelayout,    {.i = +1} },
-	{ CAPS,                    XKB_KEY_f,           cyclelayout,    {.i = -1} },
+    { CAPS,                    Key_r,           cyclelayout,    {.i = +1} },
+	{ CAPS,                    Key_f,           cyclelayout,    {.i = -1} },
     
 
     // change focused client 
-	{ SUPER,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-	{ SUPER,                    XKB_KEY_k,          focusstack,     {.i = -1} },
+	{ SUPER,                    Key_j,          focusstack,     {.i = +1} },
+	{ SUPER,                    Key_k,          focusstack,     {.i = -1} },
     
     // move clients in stack
-    { SUPER|SHIFT,              XKB_KEY_J,          movestack,      {.i = +1} },
-    { SUPER|SHIFT,              XKB_KEY_K,          movestack,      {.i = -1} },
+    { SUPER|SHIFT,              Key_j,          movestack,      {.i = +1} },
+    { SUPER|SHIFT,              Key_k,          movestack,      {.i = -1} },
 
     // client keys
-	{ SUPER,                    XKB_KEY_f,          togglefullscreen, {0} },
-	{ SUPER|SHIFT,              XKB_KEY_C,          killclient,     {0} },
-    { SUPER|SHIFT,              XKB_KEY_space,      togglefloating, {0} },
-    //{SUPER,                     XKB_KEY_t,          always on top
-	{ SUPER,                    XKB_KEY_s,          togglesticky,   {0} },
-    { SUPER,                    XKB_KEY_m,          togglemaximilized, {0} },
+	{ SUPER,                    Key_f,          togglefullscreen, {0} },
+	{ SUPER|SHIFT,              Key_c,          killclient,     {0} },
+    { SUPER|SHIFT,              Key_space,      togglefloating, {0} },
+    //{SUPER,                     Key_t,          always on top
+	{ SUPER,                    Key_s,          togglesticky,   {0} },
+    { SUPER,                    Key_m,          togglemaximilized, {0} },
     
     // screenshot
-    { CAPS,                     XKB_KEY_z,          simplespawn,    {.v = "grim - | wl-copy" } },
-    { CAPS,                     XKB_KEY_c,          simplespawn,    {.v = "grim -g \"$(slurp)\" - | wl-copy" } },
+    { CAPS,                     Key_z,          simplespawn,    {.v = "grim - | wl-copy" } },
+    { CAPS,                     Key_c,          simplespawn,    {.v = "grim -g \"$(slurp)\" - | wl-copy" } },
 
 
     // power
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_P,          simplespawn,    {.v = "loginctl poweroff" } },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_R,          simplespawn,    {.v = "loginctl reboot" } },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_S,          simplespawn,    {.v = "playerctl pause -a; loginctl suspend && swaylock" } },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_H,          simplespawn,    {.v = "loginctl hibernate" } },
+	{ SUPER|CTRL|SHIFT,         Key_p,          simplespawn,    {.v = "loginctl poweroff" } },
+	{ SUPER|CTRL|SHIFT,         Key_r,          simplespawn,    {.v = "loginctl reboot" } },
+	{ SUPER|CTRL|SHIFT,         Key_s,          simplespawn,    {.v = "playerctl pause -a; loginctl suspend && swaylock" } },
+	{ SUPER|CTRL|SHIFT,         Key_h,          simplespawn,    {.v = "loginctl hibernate" } },
 
 
     // turn off screens
-	{ CAPS,                     XKB_KEY_v,          simplespawn,    {.v = "$HOME/.config/dwl/dpms-off/target/release/dpms-off" } },
+	{ CAPS,                     Key_v,          simplespawn,    {.v = "$HOME/.config/dwl/dpms-off/target/release/dpms-off" } },
 
     // dwl
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_Q,          quit,           {0} },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_N,          simplespawn,    {.v = "alacritty -e $HOME/.config/dwl/dwl-dotfiles/scripts/makeandexit.sh" } },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_M,          restartdwl,     {0} },
+	{ SUPER|CTRL|SHIFT,         Key_q,          quit,           {0} },
+	{ SUPER|CTRL|SHIFT,         Key_n,          simplespawn,    {.v = "alacritty -e $HOME/.config/dwl/dwl-dotfiles/scripts/makeandexit.sh" } },
+	{ SUPER|CTRL|SHIFT,         Key_m,          restartdwl,     {0} },
 
     // locking
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_L,          simplespawn,    {.v = "playerctl pause -a; swaylock" } },
-	{ SUPER|CTRL|SHIFT,         XKB_KEY_K,          simplespawn,    {.v = "playerctl pause -a; $HOME/.config/dwl/dpms-off/target/release/dpms-off && swaylock" } },
+	{ SUPER|CTRL|SHIFT,         Key_l,          simplespawn,    {.v = "playerctl pause -a; swaylock" } },
+	{ SUPER|CTRL|SHIFT,         Key_k,          simplespawn,    {.v = "playerctl pause -a; $HOME/.config/dwl/dpms-off/target/release/dpms-off && swaylock" } },
 
-    { CAPS,                     XKB_KEY_2,          togglekeyboardlayout, {0} },
+    { CAPS,                     Key_2,          togglekeyboardlayout, {0} },
 
 
 #define CHVT(n) { CTRL|ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
--- dwl.c
+++ dwl.c
@@ -142,7 +142,7 @@ struct Client {
 
 typedef struct {
 	uint32_t mod;
-	xkb_keysym_t keysym;
+	xkb_keycode_t keycode;
 	void (*func)(const Arg *);
 	const Arg arg;
 } Key;
@@ -151,9 +151,8 @@ typedef struct {
 	struct wl_list link;
 	struct wlr_keyboard *wlr_keyboard;
 
-	int nsyms;
-	const xkb_keysym_t *keysyms; /* invalid if nsyms == 0 */
-	uint32_t mods; /* invalid if nsyms == 0 */
+	xkb_keycode_t keycode;
+	uint32_t mods; /* invalid if keycode == 0 */
 	struct wl_event_source *key_repeat_source;
 
 	struct wl_listener modifiers;
@@ -322,7 +321,7 @@ static void incogaps(const Arg *arg);
 static void incohgaps(const Arg *arg);
 static void incovgaps(const Arg *arg);
 static void inputdevice(struct wl_listener *listener, void *data);
-static int keybinding(uint32_t mods, xkb_keysym_t sym);
+static int keybinding(uint32_t mods, xkb_keycode_t keycode);
 static void keypress(struct wl_listener *listener, void *data);
 static void keypressmod(struct wl_listener *listener, void *data);
 static int keyrepeat(void *data);
@@ -2112,7 +2111,7 @@ inputdevice(struct wl_listener *listener, void *data)
 }
 
 int
-keybinding(uint32_t mods, xkb_keysym_t sym)
+keybinding(uint32_t mods, xkb_keycode_t keycode)
 {
 	/*
 	 * Here we handle compositor keybindings. This is when the compositor is
@@ -2123,7 +2122,7 @@ keybinding(uint32_t mods, xkb_keysym_t sym)
 	const Key *k;
 	for (k = keys; k < END(keys); k++) {
 		if (CLEANMASK(mods) == CLEANMASK(k->mod) &&
-				sym == k->keysym && k->func) {
+				keycode == k->keycode && k->func) {
 			k->func(&k->arg);
 			handled = 1;
 		}
@@ -2134,17 +2133,12 @@ keybinding(uint32_t mods, xkb_keysym_t sym)
 void
 keypress(struct wl_listener *listener, void *data)
 {
-	int i;
 	/* This event is raised when a key is pressed or released. */
 	Keyboard *kb = wl_container_of(listener, kb, key);
 	struct wlr_keyboard_key_event *event = data;
 
 	/* Translate libinput keycode -> xkbcommon */
 	uint32_t keycode = event->keycode + 8;
-	/* Get a list of keysyms based on the keymap for this keyboard */
-	const xkb_keysym_t *syms;
-	int nsyms = xkb_state_key_get_syms(
-			kb->wlr_keyboard->xkb_state, keycode, &syms);
 
 	int handled = 0;
 	uint32_t mods = wlr_keyboard_get_modifiers(kb->wlr_keyboard);
@@ -2155,17 +2149,15 @@ keypress(struct wl_listener *listener, void *data)
 	 * attempt to process a compositor keybinding. */
 	if (!locked && !input_inhibit_mgr->active_inhibitor
 			&& event->state == WL_KEYBOARD_KEY_STATE_PRESSED)
-		for (i = 0; i < nsyms; i++)
-			handled = keybinding(mods, syms[i]) || handled;
+		handled = keybinding(mods, keycode);
 
 	if (handled && kb->wlr_keyboard->repeat_info.delay > 0) {
 		kb->mods = mods;
-		kb->keysyms = syms;
-		kb->nsyms = nsyms;
+		kb->keycode = keycode;
 		wl_event_source_timer_update(kb->key_repeat_source,
 				kb->wlr_keyboard->repeat_info.delay);
 	} else {
-		kb->nsyms = 0;
+		kb->keycode = 0;
 		wl_event_source_timer_update(kb->key_repeat_source, 0);
 	}
 
@@ -2184,15 +2176,6 @@ keypressmod(struct wl_listener *listener, void *data)
 	 * pressed. We simply communicate this to the client. */
 	Keyboard *kb = wl_container_of(listener, kb, modifiers);
 
-    /* Make sure the keyboard layout is qwerty when pressing a modkey */
-    uint32_t mods = wlr_keyboard_get_modifiers(kb->wlr_keyboard);
-    if (mods == 0) {
-        setkeyboardlayout(prev_keyboard_layout_index);
-    } else if (mods != 1) {
-        prev_keyboard_layout_index = selected_keyboard_layout_index;
-        setkeyboardlayout(0);
-    }
-
 	/*
 	 * A seat can only have one keyboard, but this is a limitation of the
 	 * Wayland protocol - not wlroots. We assign all connected keyboards to the
@@ -2209,13 +2192,11 @@ int
 keyrepeat(void *data)
 {
 	Keyboard *kb = data;
-	int i;
-	if (kb->nsyms && kb->wlr_keyboard->repeat_info.rate > 0) {
+	if (kb->keycode && kb->wlr_keyboard->repeat_info.rate > 0) {
 		wl_event_source_timer_update(kb->key_repeat_source,
 				1000 / kb->wlr_keyboard->repeat_info.rate);
 
-		for (i = 0; i < kb->nsyms; i++)
-			keybinding(kb->mods, kb->keysyms[i]);
+		keybinding(kb->mods, kb->keycode);
 	}
 
 	return 0;
